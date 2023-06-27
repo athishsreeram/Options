0d088057568a47e08ab65004f0b528459feef5ea
athishsreeram <athishsreeram007@gmail.com>
2023-06-27 16:47:54 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Options
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-p6jtaz)
---
+++ src/App.tsx
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from 'react';
+import './style.css';
+
+const App = () => {
+  const [showPopup, setShowPopup] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(null);
+  const [question, setQuestion] = useState('My Question');
+  const [options, setOptions] = useState([
+    { id: '1', label: 'Option 1' },
+    { id: '2', label: 'Option 2' },
+    { id: '3', label: 'Option 3' },
+    { id: '4', label: 'Option 4' },
+  ]);
+
+  useEffect(() => {
+    fetchCaptcha();
+  }, []);
+
+  const fetchCaptcha = async () => {
+    try {
+      const response = await fetch('/captcha');
+      const data = await response.json();
+      const { question, options } = data;
+      setQuestion(question);
+      setOptions(options);
+    } catch (error) {
+      console.error('Error fetching captcha:', error);
+    }
+  };
+
+  const handleDivClick = () => {
+    setShowPopup(true);
+  };
+
+  const handleClosePopup = () => {
+    setShowPopup(false);
+    setSelectedOption(null);
+  };
+
+  const handleOptionClick = (option) => {
+    console.log(option);
+    setSelectedOption(option);
+    handleClosePopup();
+  };
+
+  return (
+    <div className="app">
+      <div className="div-container" onClick={handleDivClick}>
+        Click Me
+      </div>
+
+      {showPopup && (
+        <div className="popup">
+          <div className="popup-content">
+            <div className="question">{question}</div>
+            <div className="options">
+              <div className="grid-container">
+                {options.map((option, index) => (
+                  <img
+                    key={index}
+                    src={option.image}
+                    alt={option.label}
+                    className={`grid-item ${
+                      selectedOption === option.label ? 'selected' : ''
+                    }`}
+                    onClick={() => handleOptionClick(option.label)}
+                  />
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
